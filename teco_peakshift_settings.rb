0b34fab003f732f48ab9705e57608f751580e3e9
Claudio Vicari <cvicari@users.noreply.github.com>
2015-03-14 01:08:13 +0100

    separating registry file and log to stdout

--- teco_peakshift_settings.rb
+++ teco_peakshift_settings.rb
@@ -1,5 +1,6 @@
 require 'yaml'
 require 'set'
+require 'logger'
 
 # options to implement:
 # (*) backup peak shift values to a .reg file
@@ -30,17 +31,19 @@ class HolidayManager
 	end
 end
 
-def printPreamble()
-	puts <<-EOF
+def printPreamble(registryFile)
+	registryFile.puts <<-EOF
 	Windows Registry Editor Version 5.00
 
 	[HKEY_LOCAL_MACHINE\\SOFTWARE\\Toshiba\\eco Utility\\PeakShift]
-	"NotifyTime"=dword:0000003c
-	"IsSupport"=dword:00000001
-	"PeakShiftCount"=dword:00000004
-	"IsNotify"=dword:00000001
+	"NotifyTime" = dword:0000003c
+	"IsSupport" = dword:00000001
+	"PeakShiftCount" = dword:00000004
+	"IsNotify" = dword:00000001
 	EOF
 end
+
+ARGV[0] or abort "Usage: #{__FILE__} <file.reg>\nSettings must be in file teco_peakshift_settings.yaml"
 
 YamlProps = YAML.load_file("teco_peakshift_settings.yaml")
 Additional_holidays = YamlProps["additional_holidays"]
@@ -53,32 +56,45 @@ MinCharge   = YamlProps["minCharge"]
 
 Active      = 1
 
-printPreamble()
-
 holidayManager = HolidayManager.new(Weekend, Additional_holidays, Date.today)
 
-for i in 1..4 do
-	startTime = holidayManager.getNextWorkBegin
-	endTime = holidayManager.getNextWorkEnd
+@log = Logger.new(STDOUT)
+@log.level = Logger::INFO
 
-	puts "startTime: #{startTime}, endTime: #{endTime}"
+if (@log.level != Logger::DEBUG)
+	@log.formatter = proc do |severity, datetime, progname, msg|
+		"#{msg}\n"
+	end
+end
 
-	startMonth  = startTime.month
-	startDay    = startTime.day
-	endMonth    = endTime.month
-	endDay      = endTime.day
+File.open("#{ARGV[0]}", "w") do |registryFile|
+	@log.info "Creating registry file #{ARGV[0]}"
 
-	puts <<-EOF
-	"PeakShift#{i}"=hex: \\
-					  #{startMonth.to_s(16)},00,00,00, \\
-					    #{startDay.to_s(16)},00,00,00, \\
-					    #{endMonth.to_s(16)},00,00,00, \\
-					      #{endDay.to_s(16)},00,00,00, \\
-					   #{StartHour.to_s(16)},00,00,00, \\
-					 #{StartMinute.to_s(16)},00,00,00, \\
-					     #{EndHour.to_s(16)},00,00,00, \\
-					   #{EndMinute.to_s(16)},00,00,00, \\
-					   #{MinCharge.to_s(16)},00,00,00, \\
-					      #{Active.to_s(16)},00,00,00
-	EOF
+	printPreamble(registryFile)
+	for i in 1..4 do
+		startTime = holidayManager.getNextWorkBegin
+		endTime = holidayManager.getNextWorkEnd
+
+		@log.info "Week number #{i}, start: #{startTime}, end: #{endTime}"
+
+		startMonth  = startTime.month
+		startDay    = startTime.day
+		endMonth    = endTime.month
+		endDay      = endTime.day
+
+		registryFile.puts <<-EOF
+		"PeakShift#{i}"=hex: \\
+						  #{startMonth.to_s(16)},00,00,00, \\
+							#{startDay.to_s(16)},00,00,00, \\
+							#{endMonth.to_s(16)},00,00,00, \\
+							  #{endDay.to_s(16)},00,00,00, \\
+						   #{StartHour.to_s(16)},00,00,00, \\
+						 #{StartMinute.to_s(16)},00,00,00, \\
+							 #{EndHour.to_s(16)},00,00,00, \\
+						   #{EndMinute.to_s(16)},00,00,00, \\
+						   #{MinCharge.to_s(16)},00,00,00, \\
+							  #{Active.to_s(16)},00,00,00
+		EOF
+	end
+	@log.info "Registry file #{ARGV[0]} created, now please import it with regedit.exe"
 end
