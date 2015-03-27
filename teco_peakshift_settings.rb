00755967c2ba6520574d6ccb49fb0781f8ab3c91
Claudio Vicari <cvicari@users.noreply.github.com>
2015-03-28 00:44:59 +0100

    check for teco utility installed; command line options

--- teco_peakshift_settings.rb
+++ teco_peakshift_settings.rb
@@ -1,10 +1,10 @@
 require 'yaml'
 require 'set'
 require 'logger'
+require 'win32/registry'
+require 'getoptlong'
 
 # options to implement:
-# (*) backup peak shift values to a .reg file
-# => reg export "HKEY_LOCAL_MACHINE\\SOFTWARE\\Toshiba\\eco Utility\\PeakShift" backup.reg
 # (*) check teco version on exe, check registry key presence
 
 class HolidayManager
@@ -42,7 +42,66 @@ def printPreamble(registryFile)
 	EOF
 end
 
-ARGV[0] or abort "Usage: #{__FILE__} <file.reg>\nSettings must be in file teco_peakshift_settings.yaml"
+def checkRegistryKeyPresence
+	begin
+		# fails if the registry keys that should be present because of "Toshiba Eco Utility" are not there
+		Win32::Registry::HKEY_LOCAL_MACHINE.open "SOFTWARE2\\Toshiba\\eco Utility\\PeakShift"
+	rescue Win32::Registry::Error
+		raise "Peakshift registry key not present"
+	end
+end
+
+def checkToshibaEcoUtilityPresence
+	begin
+		key = Win32::Registry::HKEY_LOCAL_MACHINE.open "SOFTWARE\\Toshiba\\eco Utility"
+		installDir = key['InstallDir']
+		File.exists? "#{installDir}/Teco.exe" or raise 'it seems like toshiba eco utility is not installed'
+	rescue Win32::Registry::Error
+		raise "Toshiba eco utility install dir not set"
+	end
+end
+
+def winRegBackupAndImport
+	# TODO: if using windows
+	require 'win32ole'
+	shell = WIN32OLE.new('Shell.Application')
+
+	# backup
+	shell.ShellExecute 'reg', 'export /y "HKEY_LOCAL_MACHINE\\SOFTWARE\\Toshiba\\eco Utility\\PeakShift" teco_backup.reg'
+
+	# import
+	# TODO: this works fine with native windows ruby versions, not with the cygwin one. Check this
+	# shell.ShellExecute 'regedit.exe', "#{Dir.pwd}/#{ARGV[0]}"
+	# command forked, can't wait for termination
+end
+
+HELP_MSG = <<-EOF
+Usage: #{__FILE__} [options] <file.reg>
+-h, --help:
+	show help
+
+--continue, -c:
+	tries to continue even if checking for toshiba eco utility presence was unsuccessful
+
+Settings must be in file teco_peakshift_settings.yaml
+EOF
+continueAnyway = false
+
+opts = GetoptLong.new(
+	[ '--help', '-h', GetoptLong::NO_ARGUMENT ],
+	[ '--continue', '-c', GetoptLong::NO_ARGUMENT ]
+)
+opts.each do |opt,arg|
+	case opt
+		when '--help'
+			puts HELP_MSG
+			exit
+		when '--continue'
+			continueAnyway = true
+	end
+end
+
+(outputReg = ARGV.shift) or abort HELP_MSG
 
 YamlProps = YAML.load_file("teco_peakshift_settings.yaml")
 Additional_holidays = YamlProps["additional_holidays"]
@@ -66,8 +125,16 @@ if (@log.level != Logger::DEBUG)
 	end
 end
 
-File.open("#{ARGV[0]}", "w") do |registryFile|
-	@log.info "Creating registry file #{ARGV[0]}"
+begin
+	checkRegistryKeyPresence
+	checkToshibaEcoUtilityPresence
+rescue => e
+	@log.error e
+	continueAnyway or raise
+end
+
+File.open(outputReg, "w") do |registryFile|
+	@log.info "Creating registry file #{outputReg}"
 
 	printPreamble(registryFile)
 	for i in 1..4 do
@@ -95,5 +162,7 @@ File.open("#{ARGV[0]}", "w") do |registryFile|
 							  #{Active.to_s(16)},00,00,00
 		EOF
 	end
-	@log.info "Registry file #{ARGV[0]} created, now please import it with regedit.exe"
+	@log.info "Registry file #{outputReg} created, now please import it with regedit.exe"
+
+	winRegBackupAndImport
 end
